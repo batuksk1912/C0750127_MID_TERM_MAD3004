ebb2f80aa6ea882d9e80a00456f5cbb94243398a
Baturay Kayaturk <baturaykayaturk@hotmail.com>
2019-02-20 18:26:47 -0500

    Initial Commit

---
+++ C0750127_MID_TERM_MAD3004/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  C0750127_MID_TERM_MAD3004
+//
+//  Created by Baturay Kayatürk on 20.02.2019.
+//  Copyright © 2019 LambtonCollege. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
